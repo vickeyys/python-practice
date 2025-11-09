3ac933fddf86fe492a272a192dac0561333f4a05
vishal bhargav <159456389+vickeyys@users.noreply.github.com>
2025-11-09 16:25:26 +0530

    Create import-re-search.py

---
+++ datatypes/import-re-search.py
@@ -0,0 +1,8 @@
+import re 
+text = "hi song my name is song and i love Song and my name is vishal is very and good bowler and batter"
+pattern = r"lnd"
+search = re.search(pattern, text, re.IGNORECASE)
+if search:
+    print(search.group())
+else:
+    print("not found")
